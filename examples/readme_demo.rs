929ae6e79ed182620e3c52d7eb2ddb378dfdcb36
Tiffany Bennett <tiffnixen@gmail.com>
2019-11-29 23:45:08 -0800

    Update readme

--- examples/readme_demo.rs
+++ examples/readme_demo.rs
@@ -1,32 +1,3 @@
-# Moxie Native
-
-**Warning: This is a work in progress and not yet usable for nontrivial applications.**
-
-This is a framework for building GUI applications written in Rust. It
-renders using Webrender, instead of relying on other UI toolkits like
-Gtk or on a web browser. This gives you control over how your
-application looks, but may lose some native look and feel.
-
-The front-facing API is built using Moxie, a framework for declaratively
-defining UI similar to React.
-
-## Features
-
-- Relatively small footprint (4.8MB for a hello world app).
-- Declaratively specify UI instead of manually managing state.
-- Not based on immediate mode UI.
-- Targetted towards real world desktop applications.
-- Styling system for specifying the appearance of elements.
-
-## Future plans
-
-- More powerful layouts (flexbox, flexgrid, etc).
-- More user input, such as text fields.
-- See here for the current roadmap: https://github.com/tiffany352/moxie-native/projects/1
-
-## Example
-
-```rs
 // Get all the types and macros needed for concise code
 use moxie_native::prelude::*;
 
@@ -87,14 +58,3 @@ fn main() {
     let runtime = moxie_native::Runtime::new(|| my_app!());
     runtime.start();
 }
-```
-
-## License
-
-Mozilla Public License, version 2.
-
-## Contributing
-
-- Agree to license your contribution under the terms of the [license](./LICENSE-MPL).
-- Follow the [code of conduct](./CODE_OF_CONDUCT.md).
-- Check the issues and projects tabs for things to work on, or ask me (@tiffany352).
